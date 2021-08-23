8cb3d99b7bdfeaa9945376dded3ba4a7ef4c507c
Benjamin Lungwitz <benj.lung@googlemail.com>
2021-08-23 18:06:24 +0200

    configurable height

--- src/PlutoGrid.jl
+++ src/PlutoGrid.jl
@@ -6,18 +6,19 @@ using DataFrames
 
 export readonly_table
 
-function make_col_defs(df; sortable=true, filterable=true)
+function make_col_defs(df; filterable=true)
     column_defs = Dict[]
 	for c in names(df)
 		
-		col_dict = Dict("field" => c, "sortable" => sortable)
+		col_dict = Dict("field" => c)
 		
+		# special types and filters for specific element types
 		if eltype(df[!, c]) <: Number
 			col_dict["type"] = "numericColumn"
 			filterable && (col_dict["filter"] = "agNumberColumnFilter")
-		else
-			col_dict["filter"] = filterable
 		end
+		# ToDo: add type / filter for dates
+
 		push!(column_defs, col_dict)
 	end
     return column_defs
@@ -41,14 +42,17 @@ Shows a non-editable table in Pluto.
 
 `pagination`: enables pagination of the table (disabled by default)
 
+`height`: vertical size of the table in Pluto in pixel (default: 600)
+
 """
-function readonly_table(df:: DataFrame; sortable:: Bool=true, filterable:: Bool=true, pagination:: Bool=false)
-	column_defs = make_col_defs(df; sortable, filterable)
+function readonly_table(df:: DataFrame; sortable:: Bool=true, filterable:: Bool=true, pagination:: Bool=false, height:: Integer=600)
+	column_defs = make_col_defs(df; filterable)
 	data = prepare_data(df)
-    return readonly_table(column_defs, data; pagination)
+    return readonly_table(column_defs, data; sortable, filterable, pagination, height)
 end
 
-readonly_table(column_defs:: AbstractVector{<: AbstractDict}, data:: AbstractVector; pagination=false) = @htl("""
+readonly_table(column_defs:: AbstractVector{<: AbstractDict}, data:: AbstractVector; 
+	sortable=true, filterable=true, pagination=false, height:: Integer=600) = @htl("""
 <html lang="en">
 <head>
     <script src="https://unpkg.com/ag-grid-community/dist/ag-grid-community.min.js"></script>
@@ -62,6 +66,10 @@ const rowData = $(data);
 const gridOptions = {
   columnDefs: columnDefs,
   rowData: rowData,
+  defaultColDef: {
+    filter: $(sortable),
+    sortable: $(filterable)
+  },
   pagination: $(pagination)
 };
 
@@ -74,7 +82,7 @@ document.addEventListener('DOMContentLoaded', () => {
 </script>
 </head>
 <body>
-    <div id="myGrid" style="height: 600px;" class="ag-theme-alpine"></div>
+    <div id="myGrid" style="height: $(height)px;" class="ag-theme-alpine"></div>
 </body>
 </html>
 """)
